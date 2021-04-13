df83bdc292c7b7169a8611fa014da753d7af3522
Balázs <balazsgorog4@gmail.com>
2021-04-13 09:30:10 +0200

    Unit testing done, filepaths refractored, saving/loading finalized
    Saving/Loading is now testable and tested

---
+++ src/test/java/fitnessTracker2/JsonHandlerTest.java
@@ -0,0 +1,16 @@
+package fitnessTracker2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonHandlerTest {
+
+    @Test
+    void load() {
+    }
+
+    @Test
+    void save() {
+    }
+}
